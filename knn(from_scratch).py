8db08b5d05d8b7996dc7a16445869af1d2785edf
Deven Chhetri <dchhetri@uncc.edu>
2017-04-09 20:47:30 -0400

    Add files via upload

---
+++ knn(from_scratch).py
@@ -0,0 +1,50 @@
+#data is breast cancer data available at uci.edu
+import numpy as np                                                      #importing required packages and modules
+import pandas as pd
+import warnings, random
+from collections import Counter
+
+def knn(dataset, predict, k=3):                                         #here, we define the main k nearest neighbor algorithm
+    dist = []
+    for group in dataset:
+        for feat in dataset[group]:
+            euc_dist = np.linalg.norm(np.array(feat)-np.array(predict)) #calculating euclidean distance using numpy
+            dist.append([euc_dist, group])
+
+    vote = [i[1] for i in sorted(dist)[:k]]                             #vote array will hold the k nearest groups from sorted dist array
+    result = Counter(vote).most_common(1)[0][0]                         #result will hold the class which is maximum in vote array
+    return result
+
+
+data = pd.read_csv('breast-cancer-wisconsin.data.txt')
+data.replace('?', -99999, inplace=True)                                 #replace missing values with -99999(outlier)
+data.drop(['samp_code_no'], 1, inplace=True)                            #dropping the first column which represents the id
+dataset = data.astype(float).values.tolist()                            #converting all value to float and then to list
+random.shuffle(dataset)                                                 #shuffling the datasset
+
+test_size = 0.25
+training_set = {2:[], 4:[]}
+testing_set = {2:[], 4:[]}
+training_data = dataset[:-int((len(dataset))*test_size)]                #segregating training and testing data
+testing_data = dataset[-int((len(dataset))*test_size):]
+
+for i in training_data:
+    training_set[i[-1]].append(i[:-1])                                  #training and testing set will be dictionary containing arrays
+
+for i in testing_data:
+    testing_set[i[-1]].append(i[:-1])
+
+match=0
+total=0
+
+for group in testing_set:
+    for each in testing_set[group]:
+        vote = knn(training_set, each, k=3)                             #result(class) for each sample will be returned to variable(vote)
+        if group == vote:
+            match +=1                                                   #counting number of matches to calculate accuracy
+        total +=1
+
+print('accuracy =', match/total)                                        #accuracy = number of matches/total number of samples
+
+
+
---
+++ knn(sklearn_library).py
@@ -0,0 +1,26 @@
+#data is breast cancer data available at uci.edu
+import numpy as np                                                                  #importing required packages and modules
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing, neighbors
+
+data = pd.read_csv('breast-cancer-wisconsin.data.txt')
+
+data.replace('?', -99999, inplace=True)                                             #replace missing values with -99999(outlier)
+data.drop(['samp_code_no'], 1, inplace=True)                                        #dropping the first column which represents the id
+
+
+X_vals = np.array(data.drop(['class'], 1))                                          #converting to numpy arrays and seperating x from y(class)
+y_vals = np.array(data['class'])
+
+X_train, X_test, y_train, y_test = train_test_split(X_vals, y_vals, test_size=0.2)  #sepearating training and testing data
+
+classifier = neighbors.KNeighborsClassifier()                                       #defining classifier as k nearest neighbor
+classifier.fit(X_train, y_train)                                                    #training classifier on training data
+
+accuracy = classifier.score(X_test, y_test)                                         #testing
+print(accuracy)
+
+eg_data = np.array([[3,4,2,2,3,1,1,1,1], [3,4,5,6,7,1,2,2,1], [1,2,3,4,5,6,7,8,9]]) #creating new data to predict  on trained classifier
+
+print(classifier.predict(eg_data))                                                  #result on new data
